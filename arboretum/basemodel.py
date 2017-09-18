8889bd32d6e89f159c4f33fc6a9f4a517ae53837
David Thaler <davidthaler@gmail.com>
2017-09-18 00:08:47 -0700

    added doc in basemodel

--- arboretum/basemodel.py
+++ arboretum/basemodel.py
@@ -1,7 +1,13 @@
 '''
 Some functionality common to all arboretum models: get_params/
 set_params, which are needed for compatability with sklearn.model_selection
-and __repr__.
+and __repr__. 
+
+This class requires its subclasses to follow the convention from sklearn that 
+all estimator configuration paramters are keyword arguments of the constructor.
+That allows cloning "model" like this:
+
+    model.__class__(**model.get_params())
 
 author: David Thaler
 date: Septmenber 2017
@@ -16,12 +22,24 @@ class BaseModel:
         pass
 
     def get_params(self):
+        '''
+        Generate a dict of all configuration params used by this model.
+
+        Returns:
+            dict of configuration params and their values
+        '''
         sig = inspect.signature(self.__init__)
         names = list(sig.parameters.keys())
         params = {n:getattr(self, n) for n in names}
         return params
 
     def __repr__(self):
+        '''
+        Output model name and configuration.
+
+        Returns:
+            self
+        '''
         name  = self.__class__.__name__
         params = self.get_params()
         params_list = ['{}={}'.format(*t) for t in params.items()]

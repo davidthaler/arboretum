c9184a36f519ed2daeb099b303ee8f95fdcfc195
David Thaler <davidthaler@gmail.com>
2017-09-25 20:22:05 -0700

    Moved GBClassifier into gbm.py and fixed up __init__.py.

--- arboretum/__init__.py
+++ arboretum/__init__.py
@@ -3,4 +3,4 @@ Bring module functions and classes up to package namespace.
 '''
 from .tree import Tree, RegressionTree, ClassificationTree
 from .forest import Forest, RFClassifier, RFRegressor
-from .gbm import GBM
+from .gbm import GBRegressor, GBClassifier

0d81a665f3f870270b2cdfc02c077559274cc22f
David Thaler <davidthaler@gmail.com>
2017-09-30 19:44:24 -0700

    Committing some more work on the gini_splitter tests.

--- arboretum/tests/test_gini_splitter.py
+++ arboretum/tests/test_gini_splitter.py
@@ -1,84 +1,88 @@
 import unittest
 import numpy as np
-import numpy.testing as nt
 from sklearn.tree import DecisionTreeClassifier
 from ..datasets import load_iris
 from ..gini_splitter import split
 from .. import tree_constants as tc
 
+# Hand-built toy data
+
+A = np.array([[2., 2., 3., 3., 2.], 
+             [2., 3., 3., 4., 2.], 
+             [2., 2., 3., 4., 3.],
+             [2., 3., 4., 4., 3.]])
+
+B = np.array([0., 0., 1., 1.])
+W = np.ones_like(B)
+NO_SPLIT = (tc.NO_FEATURE, tc.NO_THR)
+PLACES = 4
+IRIS = load_iris()
 
 class TestGiniSplitter(unittest.TestCase):
     '''
-    First we test the gini_splitter on a tiny hand-checked toy problem.
-    Then we try it on real data, using a depth 1 sklearn DecisionTreeClassifier
+    First test the gini_splitter on a tiny hand-checked toy problem.
+    Then test it on real data, using a depth 1 sklearn DecisionTreeClassifier
     for a ground truth. In doing so, we have to force unique splits, because 
     otherwise the result is non-deterministic.
     '''
-
-    A = np.array([[2., 2., 3., 3., 2.], 
-                 [2., 3., 3., 4., 2.], 
-                 [2., 2., 3., 4., 3.],
-                 [2., 3., 4., 4., 3.]])
-    B = np.array([0., 0., 1., 1.])
-    W = np.ones_like(B)
-    NO_SPLIT = (tc.NO_FEATURE, tc.NO_THR)
-
 
     # Basic tests on the hand-built data
 
     def test_one_row(self):
         '''Test for no split and no error when data has only one row'''
-        f, thr = split(self.A[:1], self.B[:1], self.W[:1])[:2]
-        self.assertEqual(f, tc.NO_FEATURE)
-        self.assertEqual(thr, tc.NO_THR)
+        result = split(A[:1], B[:1], W[:1])[:2]
+        self.assertTupleEqual(result, NO_SPLIT)
 
     def test_pure_node(self):
         '''Test for no split and no error if node is pure'''
-        f, thr = split(self.A[:2], self.B[:2], self.W[:1])[:2]
-        self.assertTupleEqual((f, thr), (tc.NO_FEATURE, tc.NO_THR))
+        result = split(A[:2], B[:2], W[:1])[:2]
+        self.assertTupleEqual(result, NO_SPLIT)
 
     def test_pure_feature(self):
         '''Test for no split and no error if x is pure'''
-        f, thr = split(self.A[:, :1], self.B, self.W)[:2]
-        self.assertTupleEqual((f, thr), ((tc.NO_FEATURE, tc.NO_THR)))
+        result = split(A[:, :1], B, W)[:2]
+        self.assertTupleEqual(result, NO_SPLIT)
 
     def test_no_improve(self):
-        '''
-        Test for no split and no error when a split exists, 
-        but no improvement is possible
-        '''
-        result = split(self.A[:, :1], self.B, self.W)[:2]
-        self.assertTupleEqual(result, self.NO_SPLIT)
+        '''Test for no split when only a no-gain split exists'''
+        result = split(A[:,:2], B, W)[:2]
+        self.assertTupleEqual(result, NO_SPLIT)
+
+    def test_wtd_improve(self):
+        '''Test that split from no_improve can exist if weighted'''
+        w = np.array([1,1,1,2])
+        f, thr = split(A[:,:2], B, w)[:2]
+        self.assertEqual(f, 1)
+        self.assertAlmostEqual(thr, 2.5, places=PLACES)
 
     def test_perfect(self):
         '''Test for perfect split in column 4 with threshold of 2.5'''
-        f, thr = split(self.A, self.B, self.W)[:2]
+        f, thr = split(A, B, W)[:2]
         self.assertEqual(f, 4)
-        self.assertAlmostEqual(thr, 2.5)
+        self.assertAlmostEqual(thr, 2.5, places=PLACES)
 
     def test_imperfect(self):
         '''Test for the imperfect split in column 2'''
-        f, thr = split(self.A[:, :3], self.B, self.W)
+        f, thr = split(A[:, :3], B, W)[:2]
         self.assertEqual(f, 2)
-        self.assertAlmostEqual(thr, 3.5)
+        self.assertAlmostEqual(thr, 3.5, places=PLACES)
 
     # Checking splitter against sklearn on iris data 
-    # NB: Splitting iris data on columns 2 or 3 gives the same partition,
-    # which makes the test non-deterministic, so we use one at a time.
 
-    def stump(self, x, y, w=None):
+    def stump(self, x, y, w=None, **kwargs):
         '''This method gives us the ground truth for a split'''
-        if w is None:
-            w = np.ones_like(y)
-        dt = DecisionTreeClassifier(max_depth=1)
+        dt = DecisionTreeClassifier(max_depth=1, **kwargs)
         dt.fit(x, y, sample_weight=w)
         f = dt.tree_.feature[0]
         thr = dt.tree_.threshold[0]
         return (f, thr)
 
+    # NB: Splitting iris data on columns 2 or 3 gives the same partition,
+    # which makes the test non-deterministic, so we use one at a time.
+
     def test_success(self):
         '''Check the main success scenario on iris data.'''
-        x, y = load_iris()
+        x, y = IRIS
         w = np.ones_like(y)
         f, thr = split(x[:, :-1], y, w)[:2]
         gold = self.stump(x[:, :-1], y, w)
@@ -87,15 +91,26 @@ class TestGiniSplitter(unittest.TestCase):
         f, thr = split(x[:, [0, 1, 3]], y, w)[:2]
         gold = self.stump(x[:, [0, 1, 3]], y, w)
         self.assertEqual(f, gold[0])
-        self.assertAlmostEqual(thr, gold[1])
+        self.assertAlmostEqual(thr, gold[1], places=PLACES)
 
-    # AFAIK: the split is deterministic, given these weights
+    # AFAIK: the splits below are deterministic, given the parameters
+
     def test_weights(self):
         '''Check for same split when weights are used.'''
-        x, y = load_iris()
+        x, y = IRIS
         w = np.ones_like(y)
         w[75:] += 1
-        result = split(x, y, w)
+        result = split(x, y, w)[:2]
         gold = self.stump(x, y, w)
         self.assertEqual(result[0], gold[0])
-        self.assertAlmostEqual(result[1], gold[1])
+        self.assertAlmostEqual(result[1], gold[1], places=PLACES)
+
+    def test_min_leaf(self):
+        '''Check that the same split is found with min_leaf set'''
+        x, y = IRIS
+        x, y = x[::10], y[::10]
+        w = np.ones_like(y)
+        result = split(x, y, w, min_leaf=4)[:2]
+        gold = self.stump(x, y, w, min_samples_leaf=4)
+        self.assertEqual(result[0], gold[0])
+        self.assertAlmostEqual(result[1], gold[1], places=PLACES)
